f6f789304730ab62ca2e29eae9db2adfa29aa275
Manuel <bardockgokuxenonssj@gmail.com>
2025-01-26 08:06:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://preview.redd.it/4lhrjntqskf81.png?width=640&crop=smart&auto=webp&s=90b5855690e42d275360d1c8c216ad03d54f6af0"
+    ALIVE_MSG: process.env.ALIVE_MSG || "Hey, I'm alive >Netti"
 };
